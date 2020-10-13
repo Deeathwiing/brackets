281e2cd8d91b2612626efa6eaccbce0768585d1a
Deeathwiing <andreykorotkins@gmail.com>
2020-10-13 19:22:21 +0300

    init: Brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+// module.exports = 
+function check(str, bracketsConfig) {
+  
+  let comp;
+  while (str.length !==0) {
+    for (brackets of bracketsConfig) {
+        let temp = brackets.join('');
+        str = str.replace(temp , '');
+    }
+
+    if (str === comp) {
+      break;
+    }
+    comp = str;
+  }
+  return str.length === 0;
+
 }
+
+const config1 = [['(', ')']];
+const config2 = [['(', ')'], ['[', ']']];
+const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
+const config4 = [['|', '|']];
+const config5 = [['(', ')'], ['|', '|']];
+const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
+const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
+
+console.log(check('())(', config1));
